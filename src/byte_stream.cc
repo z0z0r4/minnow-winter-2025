c82963f066bdfec9bb288b4dd1b86a3e71fa948a
z0z0r4 <z0z0r4@outlook.com>
2026-09-07 18:48:55 +0800

    feat: byte_stream
    ❯ cmake --build build --target check0
    Test project /home/z0z0r4/minnow-winter-2025/build
    Connected to MAKE jobserver
          Start  1: compile with bug-checkers
     1/11 Test  #1: compile with bug-checkers ........   Passed    2.77 sec
          Start  2: t_webget
     2/11 Test  #2: t_webget .........................   Passed    0.34 sec
          Start  3: byte_stream_basics
     3/11 Test  #3: byte_stream_basics ...............   Passed    0.02 sec
          Start  4: byte_stream_capacity
     4/11 Test  #4: byte_stream_capacity .............   Passed    0.02 sec
          Start  5: byte_stream_one_write
     5/11 Test  #5: byte_stream_one_write ............   Passed    0.02 sec
          Start  6: byte_stream_two_writes
     6/11 Test  #6: byte_stream_two_writes ...........   Passed    0.02 sec
          Start  7: byte_stream_many_writes
     7/11 Test  #7: byte_stream_many_writes ..........   Passed    0.14 sec
          Start  8: byte_stream_stress_test
     8/11 Test  #8: byte_stream_stress_test ..........   Passed    0.09 sec
          Start 37: no_skip
     9/11 Test #37: no_skip ..........................   Passed    0.02 sec
          Start 38: compile with optimization
    10/11 Test #38: compile with optimization ........   Passed    1.21 sec
          Start 39: byte_stream_speed_test
            ByteStream throughput (pop length 4096):  0.87 Gbit/s
            ByteStream throughput (pop length 128):   0.74 Gbit/s
            ByteStream throughput (pop length 32):    0.72 Gbit/s
    11/11 Test #39: byte_stream_speed_test ...........   Passed    1.22 sec
    
    100% tests passed, 0 tests failed out of 11
    
    Total Test time (real) =   5.88 sec
    Built target check0

--- src/byte_stream.cc
+++ src/byte_stream.cc
@@ -1,56 +1,87 @@
 #include "byte_stream.hh"
 
+#include <stdexcept>
+
 using namespace std;
 
-ByteStream::ByteStream( uint64_t capacity ) : capacity_( capacity ) {}
+ByteStream::ByteStream( uint64_t capacity )
+  : capacity_( capacity ),
+    error_( false ),
+    closed_( false ),
+    bytes_pushed_( 0 ),
+    bytes_popped_( 0 ),
+    buffer_()
+{ 
+  buffer_ = std::vector<char>();
+  buffer_.resize(capacity_);
+}
 
 void Writer::push( string data )
 {
-  (void)data; // Your code here.
+  if (closed_) {
+    return;
+  }
+
+  const size_t write_len = min( static_cast<uint64_t>( data.size() ), available_capacity() );
+  if ( write_len == 0 ) {
+    return;
+  }
+
+  for (size_t i = 0; i < write_len; i++) {
+    buffer_[(writer_cursor_ + i) % capacity_] = data.data()[i];
+  }
+
+  writer_cursor_ = (writer_cursor_ + write_len) % capacity_ ;  
+  bytes_pushed_ += write_len;
 }
 
 void Writer::close()
 {
-  // Your code here.
+  closed_ = true;
 }
 
 bool Writer::is_closed() const
 {
-  return {}; // Your code here.
+  return closed_;
 }
 
 uint64_t Writer::available_capacity() const
 {
-  return {}; // Your code here.
+  return capacity_ - (bytes_pushed_ - bytes_popped_);
 }
 
 uint64_t Writer::bytes_pushed() const
 {
-  return {}; // Your code here.
+  return bytes_pushed_;
 }
 
 string_view Reader::peek() const
 {
-  return {}; // Your code here.
+  auto len = min(capacity_ - reader_cursor_, bytes_buffered());
+  return string_view(buffer_.data() + reader_cursor_, len);
 }
 
 void Reader::pop( uint64_t len )
 {
-  (void)len; // Your code here.
+  if (bytes_buffered() < len) {
+    throw runtime_error("No enough buffered bytes");
+  }
+  reader_cursor_ = (reader_cursor_ + len) % capacity_;
+  bytes_popped_ += len;
 }
 
 bool Reader::is_finished() const
 {
-  return {}; // Your code here.
+  return closed_ && bytes_popped_ == bytes_pushed_;
 }
 
 uint64_t Reader::bytes_buffered() const
 {
-  return {}; // Your code here.
+  return bytes_pushed_ - bytes_popped_;
 }
 
 uint64_t Reader::bytes_popped() const
 {
-  return {}; // Your code here.
+  return bytes_popped_;
 }
 
--- src/byte_stream.hh
+++ src/byte_stream.hh
@@ -3,6 +3,7 @@
 #include <cstdint>
 #include <string>
 #include <string_view>
+#include <vector>
 
 class Reader;
 class Writer;
@@ -25,6 +26,15 @@ protected:
   // Please add any additional state to the ByteStream here, and not to the Writer and Reader interfaces.
   uint64_t capacity_;
   bool error_ {};
+  bool closed_ {};
+
+  uint64_t bytes_pushed_ {};
+  uint64_t bytes_popped_ {};
+
+  uint64_t writer_cursor_ {};
+  uint64_t reader_cursor_ {};
+
+  std::vector<char> buffer_;
 };
 
 class Writer : public ByteStream
